a0e97dd17265361101bf3fb7cc22dc4e3dbe1298
yuanjin <yuanjinmoke@gmail.com>
2020-01-07 08:53:38 +0800

    1-3. 模块化兼容性

---
+++ 1. webpack核心功能/1-3. 模块化兼容性/src/es6a.js
@@ -0,0 +1,9 @@
+// export var a = 1;
+// export var b = 2;
+// export default 3;
+
+export default {
+    a: 1,
+    b: 2,
+    c: 3
+}
---
+++ 1. webpack核心功能/1-3. 模块化兼容性/src/index.js
@@ -0,0 +1,19 @@
+// var obj = require("./es6a")
+// console.log(obj.a, obj.b, obj.default)
+
+// import * as obj from "./commonjsa"
+// import obj from "./commonjsa"
+
+// console.log(obj)
+
+//jquery
+//module.exports = function(){}
+
+// var $ = require("jquery")
+// console.log($)
+
+// import $ from "jquery"
+// console.log($);
+
+var obj = require("./es6a")
+console.log(obj)
